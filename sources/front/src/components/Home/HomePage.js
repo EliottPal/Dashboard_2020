01092fffd518996c9bccdcac0dd0d97601585bd8
Eliott Pal <eliott.palueau@epitech.eu>
2020-11-12 12:13:38 +0100

    [Front] -> Home Page -> creating draggable cards & first basic design

--- sources/front/src/components/Home/HomePage.js
+++ sources/front/src/components/Home/HomePage.js
@@ -3,9 +3,11 @@ import React, { useState } from 'react';
 import { Link } from '@reach/router';
 import { makeStyles } from "@material-ui/core/styles";
 import defaultImg from './../../assets/bg.jpg'
-import Fab from '@material-ui/core/Fab';
+import Draggable from 'react-draggable';
+import {Card, Button, Typography, Fab } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 import PersonIcon from '@material-ui/icons/Person'
+import CloseIcon from '@material-ui/icons/Close';
 
 const useStyles = makeStyles((theme) => ({
     // Buttons
@@ -20,13 +22,58 @@ const useStyles = makeStyles((theme) => ({
         left: '47%',
         marginTop: theme.spacing(1),
     },
+    destroyButton: {
+        position: 'absolute',
+        height: '4vh',
+        width: '4vh',
+        right: '94%',
+        top: '-8%',
+    },
     // Icons
-    profileIcon: {
+    largerIcon: {
         height: '5vh',
         width: '5vh',
+    },
+    smallerIcon: {
+        height: '2vh',
+        width: '2vh',
+    },
+    // Card
+    card: {
+        width: '20%',
+        minHeight: '20vh',
+        backgroundColor: '#f5f5f5',
+        color: '#00000',
+        textAlign: 'center',
+        overflow: 'visible',
     }
 }));
 
+/**
+ * Material-UI Card that you can drag and drop anywhere.
+ */
+const DraggableCard = ({ text}) => {
+    const classes = useStyles();
+
+    const destroyWidget = async () => {
+        alert('vous me le bannez lui!');
+    };
+
+    return (
+      <Draggable>
+        <Card className={classes.card}>
+            <Fab
+                color="secondary"
+                className={classes.destroyButton}
+                onClick={(event) => destroyWidget()}
+            >
+                <CloseIcon className={classes.smallerIcon}/>
+            </Fab>
+          <Typography variant="h6">{text}</Typography>
+        </Card>
+      </Draggable>
+    );
+  };
 
 function HomePage(props) {
     const classes = useStyles();
@@ -45,7 +92,7 @@ function HomePage(props) {
                     className={classes.profileButton}
                     component={Link} to={"/profile"}
                 >
-                    <PersonIcon className={classes.profileIcon}/>
+                    <PersonIcon className={classes.largerIcon}/>
                 </Fab>
                 <h1 className="mainTitle">{date.toDateString()}</h1>
             </div>
@@ -56,6 +103,8 @@ function HomePage(props) {
             >
                 <AddIcon />
             </Fab>
+            <DraggableCard text="Eliott"></DraggableCard>
+            <DraggableCard text="Rodo"></DraggableCard>
         </div>
     );
 };
