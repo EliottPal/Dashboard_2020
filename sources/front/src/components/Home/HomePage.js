bb4e5eb1e0f26d580007d13f3cce83f5e98471db
Eliott Pal <eliott.palueau@epitech.eu>
2020-11-12 21:33:54 +0100

    [Front] -> Home Page -> adding toggle delete mode buton NOT WORKING YET + adding comments

--- sources/front/src/components/Home/HomePage.js
+++ sources/front/src/components/Home/HomePage.js
@@ -1,13 +1,18 @@
 import './HomePage.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from '@reach/router';
 import { makeStyles } from "@material-ui/core/styles";
+import { StylesProvider } from "@material-ui/styles";
 import defaultImg from './../../assets/bg.jpg'
 import Draggable from 'react-draggable';
-import {Card, Button, Typography, Fab } from '@material-ui/core';
+import {Card, Typography, Fab } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 import PersonIcon from '@material-ui/icons/Person'
 import CloseIcon from '@material-ui/icons/Close';
+import DeleteIcon from '@material-ui/icons/Delete';
+import ToggleButton from '@material-ui/lab/ToggleButton';
+
+var selected
 
 const useStyles = makeStyles((theme) => ({
     // Buttons
@@ -18,16 +23,16 @@ const useStyles = makeStyles((theme) => ({
         height: '8vh',
         width: '8vh',
     },
-    addButton: {
-        left: '47%',
-        marginTop: theme.spacing(1),
-    },
     destroyButton: {
         position: 'absolute',
         height: '4vh',
         width: '4vh',
         right: '94%',
         top: '-8%',
+    },
+    addButton: {
+        marginLeft: '1vh',
+        marginRight: '3vh',
     },
     // Icons
     largerIcon: {
@@ -37,6 +42,9 @@ const useStyles = makeStyles((theme) => ({
     smallerIcon: {
         height: '2vh',
         width: '2vh',
+    },
+    whiteIcon: {
+        color: '#ffffff'
     },
     // Card
     card: {
@@ -49,63 +57,85 @@ const useStyles = makeStyles((theme) => ({
     }
 }));
 
-/**
- * Material-UI Card that you can drag and drop anywhere.
- */
-const DraggableCard = ({ text}) => {
+// DRAGGABLE CARD
+const DraggableCard = ({ text }) => {
     const classes = useStyles();
 
     const destroyWidget = async () => {
-        alert('vous me le bannez lui!');
+            alert('vous me le bannez lui!');
     };
-
     return (
       <Draggable>
         <Card className={classes.card}>
-            <Fab
-                color="secondary"
-                className={classes.destroyButton}
-                onClick={(event) => destroyWidget()}
-            >
-                <CloseIcon className={classes.smallerIcon}/>
-            </Fab>
+            {selected? null :
+                <Fab
+                    color="secondary"
+                    className={classes.destroyButton}
+                    onClick={() => destroyWidget()}
+                >
+                    <CloseIcon className={classes.smallerIcon}/>
+                </Fab>
+            }
           <Typography variant="h6">{text}</Typography>
         </Card>
       </Draggable>
     );
   };
 
+// HOME PAGE
 function HomePage(props) {
     const classes = useStyles();
     var date = new Date();
+    const [selected, setSelected] = React.useState(false);
 
+    // CREATE WIDGET FUNC
     const createWidget = async (event) => {
         alert('c\'est un gaté lui ?');
     };
 
+    // TOGGLE DELETE MODE FUNC
+    const toggleDeleteMode = async (event) => {
+    };
+
     return (
-        <div className="mainDiv">
-            {/* Image configurable par l'user par la suite */}
-            <div className="imgCover" style={{ background: `url(${defaultImg})`}}>
-                <Fab
-                    color="primary"
-                    className={classes.profileButton}
-                    component={Link} to={"/profile"}
-                >
-                    <PersonIcon className={classes.largerIcon}/>
-                </Fab>
-                <h1 className="mainTitle">{date.toDateString()}</h1>
+        <StylesProvider injectFirst>
+            <div className="mainDiv">
+                {/* Image configurable par l'user par la suite */}
+                <div className="imgCover" style={{ background: `url(${defaultImg})`}}>
+                    {/* PROFILE BUTTON */}
+                    <Fab
+                        color="primary"
+                        className={classes.profileButton}
+                        component={Link} to={"/profile"}
+                    >
+                        <PersonIcon className={classes.largerIcon}/>
+                    </Fab>
+                    <h1 className="mainTitle">{date.toDateString()}</h1>
+                </div>
+                <div className="widgetManageButtons">
+                    {/* TOGGLE DELETE MODE BUTTON */}
+                    <ToggleButton
+                        value="check"
+                        selected={selected}
+                        onChange={() => {setSelected(!selected);}}
+                        onClick={(event) => toggleDeleteMode(event)}
+                        className="toggleBtn"
+                    >
+                        <DeleteIcon className={classes.whiteIcon}/>
+                    </ToggleButton>
+                    {/* ADD WIDGET BUTTON */}
+                    <Fab
+                        color="primary"
+                        className={classes.addButton}
+                        onClick={(event) => createWidget(event)}
+                    >
+                        <AddIcon />
+                    </Fab>
+                </div>
+                <DraggableCard text="Eliott"></DraggableCard>
+                <DraggableCard text="Rodo"></DraggableCard>
             </div>
-            <Fab
-                color="primary"
-                className={classes.addButton}
-                onClick={(event) => createWidget(event)}
-            >
-                <AddIcon />
-            </Fab>
-            <DraggableCard text="Eliott"></DraggableCard>
-            <DraggableCard text="Rodo"></DraggableCard>
-        </div>
+        </StylesProvider>
     );
 };
 
