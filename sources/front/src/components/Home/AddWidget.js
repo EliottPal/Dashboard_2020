03aa5d10a898a0ec67d2be0334f263614b913a7e
Rodolphe Dupuis <rodolphe.dupuis@epitech.eu>
2020-11-16 17:34:30 +0100

    add widget modal

--- sources/front/src/components/Home/AddWidget.js
+++ sources/front/src/components/Home/AddWidget.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from "@material-ui/core/styles";
 import { StylesProvider } from "@material-ui/styles";
-import { Dialog, DialogTitle, DialogContent, Typography, CardActionArea } from '@material-ui/core';
+import { Dialog, DialogTitle, DialogContent, Typography, CardActionArea, Button } from '@material-ui/core';
 import Card from '@material-ui/core/Card';
+import TextField from '@material-ui/core/TextField';
 import './AddWidget.css';
 import iconYoutube from './../../assets/icons/youtube.png'
 import iconSpotify from './../../assets/icons/spotify.png'
@@ -18,39 +19,147 @@ const useStyles = makeStyles((theme) => ({
         width: '55vh',
     },
 }));
+
+
+function WidgetSelection(props) {
+    const {title, subtitle, widget1, widget2, icon} = props;
+
+    const [openConfig, setOpenConfig] = useState(false);
+    const [type, setType] = useState(0);
+
+    const handleConfig = (newType) => {
+        setType(newType);
+        setOpenConfig(true);
+    }
+
+    return (
+        <div>
+            {openConfig === false && (
+                <div>
+                    <DialogTitle>
+                    <div className="header">
+                        <h1 className="title">{title}</h1>
+                        <h2 className="subTitle">{subtitle}</h2>
+                    </div>
+                    </DialogTitle>
+                    <DialogContent dividers>
+                        <Card className="serviceCard" variant="outlined" >
+                            <CardActionArea className="serviceCardAction" onClick={() => handleConfig(0)}>
+                                <img src={icon} className="serviceIcon"/>
+                                <div className="">
+                                    <Typography className="serviceText">{widget1}</Typography>
+                                </div>
+                            </CardActionArea>
+                        </Card>
+                        <Card className="serviceCard" variant="outlined">
+                            <CardActionArea className="serviceCardAction" onClick={() => handleConfig(1)}>
+                                <img src={icon} className="serviceIcon"/>
+                                <div className="servicePos">
+                                    <Typography className="serviceText">{widget2}</Typography>
+                                </div>
+                            </CardActionArea>
+                        </Card>
+                    </DialogContent>
+                </div>
+            )}
+            {openConfig === true && (
+                <WidgetConfig type={type} title={title} subtitle="Configuration" icon={icon} label="Name" global={title}/>
+            )}
+        </div>
+    )
+}
+
+function WidgetConfig(props) {
+    const {type, title, subtitle, icon, label, global} = props;
+    const [name, setName] = useState('');
+
+    return (
+        <div>
+            <DialogTitle>
+                <div className="header">
+                    <h1 className="title">{title}</h1>
+                    <h2 className="subTitle">{subtitle}</h2>
+                </div>
+            </DialogTitle>
+            <DialogContent dividers>
+                <Card className="serviceCard" variant="outlined">
+                    <img src={icon} className="serviceIcon"/>
+                    <TextField
+                        required
+                        variant="outlined"
+                        onChange={(e) => setName(e.target.value)}
+                        fullWidth
+                        id="name"
+                        label={label}
+                        name="name"
+                        value={name}
+                    />
+                </Card>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={() => alert("yes")}
+                >
+                    Create widget
+                </Button>
+            </DialogContent>
+        </div>
+    )
+}
+
 export default function AddWidget(props) {
-  const classes = useStyles();
-  const {openWidgetAdder, setOpenWidgetAdder} = props;
+    const classes = useStyles();
+    const {openWidgetAdder, setOpenWidgetAdder} = props;
+
+    const [showYoutube, setShowYoutube] = useState(false);
+    const [showSpotify, setShowSpotify] = useState(false);
+    const [showGithub, setShowGithub] = useState(false);
+    const [showWeather, setShowWeather] = useState(false);
+    const [showMoney, setShowMoney] = useState(false);
+
+    const [mainView, setMainView] = useState(true);
 
     const handleClose = () => {
+        setShowYoutube(false);
+        setShowSpotify(false);
+        setShowGithub(false);
+        setShowWeather(false);
+        setShowMoney(false);
         setOpenWidgetAdder(false);
+        setMainView(true);
     }
 
     // SELECT YOUTUBE SERVICE
     const handleYoutubeClick = () => {
-        alert("Youtube service");
+        setShowYoutube(true);
+        setMainView(false);
     }
 
     // SELECT SPOTIFY SERVICE
     const handleSpotifyClick = () => {
-        alert("Spotify service");
+        setShowSpotify(true);
+        setMainView(false);
     }
 
     // SELECT GITHUB SERVICE
     const handleGithubClick = () => {
-        alert("github service");
+        setShowGithub(true);
+        setMainView(false);
     }
 
     // SELECT WEATHER SERVICE
     const handleWeatherClick = () => {
-        alert("Weather service");
+        setShowWeather(true);
+        setMainView(false);
     }
 
     // SELECT MONEY SERVICE
     const handleMoneyClick = () => {
-        alert("Money service");
+        setShowMoney(true);
+        setMainView(false);
     }
 
+    console.log(mainView);
     return (
         <StylesProvider>
             <Dialog
@@ -59,61 +168,74 @@ export default function AddWidget(props) {
                 fullWidth={true}
                 classes={{paper: classes.dialogCard}}
             >
-                {/* TITLE */}
-                <DialogTitle>
-                    <div className="header">
-                        <h1 className="title">Add a new widget</h1>
-                        <h2 className="subTitle">Select service</h2>
-                    </div>
-                </DialogTitle>
-                {/* CONTENT */}
-                <DialogContent dividers>
-                    {/* YOUTUBE SERVICE */}
-                    <Card className="serviceCard" variant="outlined" >
-                        <CardActionArea className="serviceCardAction" onClick={handleYoutubeClick}>
-                            <img src={iconYoutube} className="serviceIcon"/>
-                            <div className="servicePos">
-                                <Typography className="serviceText">YouTube</Typography>
-                            </div>
-                        </CardActionArea>
-                    </Card>
-                    {/* SPOTIFY SERVICE */}
-                    <Card className="serviceCard" variant="outlined">
-                        <CardActionArea className="serviceCardAction" onClick={handleSpotifyClick}>
-                            <img src={iconSpotify} className="serviceIcon"/>
-                            <div className="servicePos">
-                                <Typography className="serviceText">Spotify</Typography>
-                            </div>
-                        </CardActionArea>
-                    </Card>
-                    {/* GITHUB SERVICE */}
-                    <Card className="serviceCard" variant="outlined">
-                        <CardActionArea className="serviceCardAction" onClick={handleGithubClick}>
-                            <img src={iconGithub} className="serviceIcon"/>
-                            <div className="servicePos">
-                                <Typography className="serviceText">Github</Typography>
-                            </div>
-                        </CardActionArea>
-                    </Card>
-                    {/* WEATHER SERVICE */}
-                    <Card className="serviceCard" variant="outlined">
-                        <CardActionArea className="serviceCardAction" onClick={handleWeatherClick}>
-                            <img src={iconWeather} className="serviceIcon"/>
-                            <div className="servicePos">
-                                <Typography className="serviceText">Weather</Typography>
-                            </div>
-                        </CardActionArea>
-                    </Card>
-                    {/* MONEY SERVICE */}
-                    <Card className="serviceCard" variant="outlined">
-                        <CardActionArea className="serviceCardAction"onClick={handleMoneyClick}>
-                            <img src={iconMoney} className="serviceIcon"/>
-                            <div className="servicePos">
-                                <Typography className="serviceText">Money</Typography>
-                            </div>
-                        </CardActionArea>
-                    </Card>
-                </DialogContent>
+                { mainView === true && (
+                    <div>
+                    {/* TITLE */}
+                    <DialogTitle>
+                        <div className="header">
+                            <h1 className="title">Add a new widget</h1>
+                            <h2 className="subTitle">Select service</h2>
+                        </div>
+                    </DialogTitle>
+                    {/* CONTENT */}
+                    <DialogContent dividers>
+                        {/* YOUTUBE SERVICE */}
+                        <Card className="serviceCard" variant="outlined" >
+                            <CardActionArea className="serviceCardAction" onClick={handleYoutubeClick}>
+                                <img src={iconYoutube} className="serviceIcon"/>
+                                <div className="servicePos">
+                                    <Typography className="serviceText">YouTube</Typography>
+                                </div>
+                            </CardActionArea>
+                        </Card>
+                        {/* SPOTIFY SERVICE */}
+                        <Card className="serviceCard" variant="outlined">
+                            <CardActionArea className="serviceCardAction" onClick={handleSpotifyClick}>
+                                <img src={iconSpotify} className="serviceIcon"/>
+                                <div className="servicePos">
+                                    <Typography className="serviceText">Spotify</Typography>
+                                </div>
+                            </CardActionArea>
+                        </Card>
+                        {/* GITHUB SERVICE */}
+                        <Card className="serviceCard" variant="outlined">
+                            <CardActionArea className="serviceCardAction" onClick={handleGithubClick}>
+                                <img src={iconGithub} className="serviceIcon"/>
+                                <div className="servicePos">
+                                    <Typography className="serviceText">Github</Typography>
+                                </div>
+                            </CardActionArea>
+                        </Card>
+                        {/* WEATHER SERVICE */}
+                        <Card className="serviceCard" variant="outlined">
+                            <CardActionArea className="serviceCardAction" onClick={handleWeatherClick}>
+                                <img src={iconWeather} className="serviceIcon"/>
+                                <div className="servicePos">
+                                    <Typography className="serviceText">Weather</Typography>
+                                </div>
+                            </CardActionArea>
+                        </Card>
+                        {/* MONEY SERVICE */}
+                        <Card className="serviceCard" variant="outlined">
+                            <CardActionArea className="serviceCardAction"onClick={handleMoneyClick}>
+                                <img src={iconMoney} className="serviceIcon"/>
+                                <div className="servicePos">
+                                    <Typography className="serviceText">Money</Typography>
+                                </div>
+                            </CardActionArea>
+                        </Card>
+                    </DialogContent>
+                </div>
+                )}
+                {showYoutube === true && (
+                    <WidgetSelection title="YouTube" subtitle="Widget list" widget1="Number of subscribers" widget2="Last video" icon={iconYoutube}/>
+                )}
+                {showSpotify === true && (
+                    <WidgetSelection title="Spotify" subtitle="Widget list" widget1="Top songs" widget2="Public playlist" icon={iconSpotify}/>
+                )}
+                {showGithub === true && (
+                    <WidgetSelection title="Github" subtitle="Widget list" widget1="Public repositories" widget2="Last commits" icon={iconGithub}/>
+                )}
             </Dialog>
         </StylesProvider>
     );
